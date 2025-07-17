59673efa84206c48ff2aade243ec97ba836dd071
ndurushima <ndu1994@gmail.com>
2025-07-17 01:20:54 -0500

    custom hooks with localStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const storedValue = localStorage.getItem(key);
+    const initial = storedValue !== null ? storedValue : initialValue;
+
+    const [value, setValue] = useState(initial);
+
+    useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [key, value]);
+
+    return [value, setValue]
 }
